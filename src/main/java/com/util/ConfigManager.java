37bc334f431d85df52d6253522cb89caa164507c
hulei <hulei_otm@163.com>
2019-01-05 16:26:09 +0800

    测试案例1

---
+++ src/main/java/com/util/ConfigManager.java
@@ -0,0 +1,56 @@
+package com.util;
+
+
+import com.constant.Constants;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+public class ConfigManager {
+    //创建properties
+    private static Properties prop=new Properties();
+    //创建静态代码块用于导入
+    static {
+        try {
+            InputStream inStream = ConfigManager.class.getClassLoader().getResourceAsStream("config.properties");
+            prop.load(inStream);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    //创建获取properties文件的方法String类型
+    public static String getProperties(String key){
+        return prop.getProperty(key);
+    }
+    //返回boolean型
+    public static Boolean getBooleanProperties(String key){
+        boolean b=false;
+        String prop= getProperties(key);
+        try {
+         b = Boolean.parseBoolean(prop);
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            return b;
+        }
+    }
+
+    //返回Integer类型
+    public static Integer getIntegerProperties(String key){
+        String prop= getProperties(key);
+        try {
+            return Integer.valueOf(prop);
+        } catch (NumberFormatException e) {
+            e.printStackTrace();
+        }finally {
+            return 0;
+        }
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println(getBooleanProperties(Constants.IS_LOCAL));
+    }
+}
